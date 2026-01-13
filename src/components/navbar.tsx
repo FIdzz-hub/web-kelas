f7170627254572e9326a97dc0b4453a41624403a
UserHan9 <raihanindra712@gmail.com>
2026-01-13 12:37:06 +0700

    feat:fetch_navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,23 @@ function Navbar() {
     <nav className="fixed top-0 left-0 w-full z-50">
       <div className="flex items-center justify-between px-10 py-5 bg-transparent">
 
-
-
         <ul className="flex p-4 gap-12 text-2xl text-white font-small-caps">
-          <li className="hover:text-gray-300 cursor-pointer transition">Home</li>
-          <li className="hover:text-gray-300 cursor-pointer transition">About Us</li>
-          <li className="hover:text-gray-300 cursor-pointer transition">Gallery</li>
-          <li className="hover:text-gray-300 cursor-pointer transition">News</li>
+          <li className="relative cursor-pointer hover:text-gray-300 transition group">
+            Home
+            <span className="absolute left-0 -bottom-1 h-[2px] bg-white w-0 group-hover:w-full transition-all duration-300 ease-out"></span>
+          </li>
+          <li className="relative cursor-pointer hover:text-gray-300 transition group">
+            About Us
+            <span className="absolute left-0 -bottom-1 h-[2px] bg-white w-0 group-hover:w-full transition-all duration-300 ease-out"></span>
+          </li>
+          <li className="relative cursor-pointer hover:text-gray-300 transition group">
+            Gallery
+            <span className="absolute left-0 -bottom-1 h-[2px] bg-white w-0 group-hover:w-full transition-all duration-300 ease-out"></span>
+          </li>
+          <li className="relative cursor-pointer hover:text-gray-300 transition group">
+            News
+            <span className="absolute left-0 -bottom-1 h-[2px] bg-white w-0 group-hover:w-full transition-all duration-300 ease-out"></span>
+          </li>
         </ul>
 
       </div>
